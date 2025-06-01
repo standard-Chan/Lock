51aecfcbb6e9a78607c25fa92938b1f6c79c72fd
standard-chan <starp321@naver.com>
2025-06-01 11:36:34 +0900

    초기세팅

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.jeong"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // lombok
+    compileOnly ("org.projectlombok:lombok:1.18.36")
+    annotationProcessor ("org.projectlombok:lombok:1.18.36")
+    // lombok test
+    testCompileOnly ("org.projectlombok:lombok:1.18.36")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.36")
+
+    // spring boot
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //MYSQL
+    implementation("mysql:mysql-connector-java:8.0.33")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
